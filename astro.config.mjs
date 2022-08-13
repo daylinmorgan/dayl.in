3e89b2ce156ece314a43bed7360e7a154dd390c5
Daylin Morgan <daylinmorgan@gmail.com>
2022-08-13 12:26:49 -0500

    shrink the logo slightly

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,9 @@
 import { defineConfig } from "astro/config";
-import Unocss from "@unocss/vite";
 import { presetUno } from 'unocss'
 import uno from "astro-uno";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://dayl.in",
-  integrations: [uno({presets:[presetUno()]})],
-  // vite: {
-  // plugins: [
-  // Unocss({})
-  // ],
-  // }
+  integrations: [uno({ presets: [presetUno()] })],
 });
